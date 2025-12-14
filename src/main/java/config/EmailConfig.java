2c6aa22c29aea294c702ab493e7d6a0498b45af5
marcus98 <echtay1102@gmail.com>
2025-12-14 21:21:29 +0700

    Chore: clean project, add .gitignore & remove target foler

--- src/main/java/config/EmailConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-public class EmailConfig {
-    public static final String HOST_NAME = "smtp.gmail.com";
-    public static final int SSL_PORT = 465;
-    public static final int TSL_PORT = 587;
-    public static final String APP_EMAIL = "echtay1102@gmail.com";
-    public static final String APP_PASSWORD = "dvuh hdte lhwf eexy"; 
-}
